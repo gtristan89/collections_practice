85f5e7c323efe195e61770b18951a818e7498153
Tristan Gibbs <gtristan89@gmail.com>
2021-01-20 23:15:33 -0700

    commit tree f19a1085232fcbc9ce611fe0ab26ccc84236ee9f

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
 
+
+def sort_array_asc(number)
+    number.sort 
+end
+
+def sort_array_desc(number)
+    number.sort.reverse
+end
+
+
+
+def sort_array_char_count(strings)
+    strings.sort do |a, b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements(names)
+    names[1], names[2] = names[2], names[1]
+    names
+end
+
+def reverse_array(integers)
+    integers.reverse
+end
+
+def kesha_maker(more_strings)
+    new_array = []
+    more_strings.each do |string|
+        string[2] = "$"
+    end
+end
+
+def find_a(ayyyeee)
+    ayyyeee.find_all do |letter|
+        letter[0] == "a"
+    end
+end
+
+def sum_array(sums)
+    sums.inject {|sum,number| sum + number}
+end
+
+def add_s(im_a_ssssnek)
+    im_a_ssssnek.collect do |hiss|
+        if im_a_ssssnek[1] == hiss
+            hiss
+        else
+            hiss + "s"
+        end
+    end
+end
